fa4f494d21eddb48715e210be1912264f6b668e1
Mohit Patil <95227ms@gmail.com>
2023-04-19 14:45:25 +0530

    changed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "7a900ce709mshec16b602caa8660p102ec0jsnd036c97f6c80",
+    "X-RapidAPI-Key": "28a9cbde06msh24cac9b2488673dp187feajsn4c4c2739a087",
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
